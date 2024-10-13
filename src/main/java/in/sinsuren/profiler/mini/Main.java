00f410b3647ba5b95cebee03ba108846fd946b61
Surendra Singh <surendra.singh@cred.club>
2024-10-13 09:48:34 +0530

    Refactor

--- src/main/java/in/sinsuren/profiler/mini/Main.java
+++ src/main/java/in/sinsuren/profiler/mini/Main.java
@@ -1,19 +1,19 @@
 package in.sinsuren.profiler.mini;
 
 public class Main {
-    public static void agentmain(String agentArgs) {
-        premain(agentArgs);
-    }
+  public static void agentmain(String agentArgs) {
+    premain(agentArgs);
+  }
 
-    public static void premain(String agentArgs) {
-        Main main = new Main();
-        main.run(new Options(agentArgs));
-    }
+  public static void premain(String agentArgs) {
+    Main main = new Main();
+    main.run(new Options(agentArgs));
+  }
 
-    private void run(Options options) {
-        Thread t = new Thread(Profiler.newInstance(options));
-        t.setDaemon(true);
-        t.setName("Profiler");
-        t.start();
-    }
+  private void run(Options options) {
+    Thread t = new Thread(Profiler.newInstance(options));
+    t.setDaemon(true);
+    t.setName("Profiler");
+    t.start();
+  }
 }
